01de39ac2f3eebaa3c388b2d915715c231f3d08c
jorge-luke <jorgestl123@gmail.com>
2024-12-18 21:41:27 -0300

    into the logics of the calculator

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 console.log("hi");
 
-//make a var for what is at the screen
-//at each number clicked, change the var for what is at the screen
+
 //when click at an operator, take it to the screen 
 // when clicked at =, make the result appear at the 'actual' line
 //
@@ -10,13 +9,49 @@ let operator = "";
 let previousValue = "";
 let actualValue = "";
 
-let clear = document.querySelector("#clear");
+let clear = document.querySelector(".clear");
 let equal = document.querySelector(".equal");
-let numbers = document.querySelectorAll("number");
-let operators = document.querySelectorAll("operator");
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
 let decimal = document.querySelector(".decimal");
 
 let previous = document.querySelector(".previous");
 let actual = document.querySelector(".actual");
+
+
+clear.addEventListener("click",()=>{
+    previous.textContent = "";
+    actual.textContent = "";
+    operator = "";
+    previousValue = "";
+    actualValue = "";vff
+})
+
+
+numbers.forEach((num)=>
+    num.addEventListener("click",(event)=>{
+        actualValue += event.target.textContent;
+        actual.textContent = actualValue;
+    }));
+
+operators.forEach(function(operator){
+    operator.addEventListener("click",(event)=>{
+        if(operator = ""){
+        operator = event.target.textContent;
+        actualValue = actualValue + operator;
+        actual.textContent = actualValue + operator.toString();
+        } else {
+        previousValue += actualValue + operator;
+        previous.textContent = previousValue + operator;
+         }
+});
+});
+
+//21 min
+
+
+
+
+
 
 
